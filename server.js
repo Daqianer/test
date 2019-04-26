fd7e90706b863e00b39c6f854709a17153d58c4f
Daqianer <daqianer1993@gmail.com>
2019-04-26 09:50:20 +0800

    change server

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const proxy = require("http-proxy-middleware");
 const history = require("connect-history-api-fallback")
 const app = express();
 
-app.use("^/lh",proxy({
-    target:"http://39.106.215.229:8084",//http://10.9.67.44:8084/
-    changeOrigin:true,
-    pathRewrite:{
-        "^/lh":"/"
-    }
-}))
+//app.use("^/lh",proxy({
+//    target:"http://39.106.215.229:8084",//http://10.9.67.44:8084/
+//    changeOrigin:true,
+//    pathRewrite:{
+//        "^/lh":"/"
+//    }
+//}))
 // url重写  将所有的请求指向index.html
 app.use(history(
     {
